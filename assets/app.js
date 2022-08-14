109e3c37cc6bb29ac076889355d3fed87d9c3b39
MohamedSelimMaazouz993 <mohamedselim.maazouz@gmail.com>
2022-08-14 18:20:14 +0100

    react injection to the project

---
+++ assets/app.js
@@ -0,0 +1,22 @@
+import './styles/app.css';
+import './bootstrap';
+import Naavbar from './components/Naavbar'
+ 
+
+
+
+const App = () => {
+  return (
+    <>    
+      <Naavbar/>
+    <main className='py-3'>
+    
+    </main>
+    <Footer/>
+    </>
+
+  )
+}
+
+
+export default App;
---
+++ assets/components/Naavbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Naavbar() {
+  return (
+    <div>Naavbar</div>
+  )
+}
